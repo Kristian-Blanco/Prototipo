247d2bb3d3512e7d67a2c5f22cbf842fa4c35007
AndreaLozanoCortes <alozanoc@gmail.com>
2022-09-05 19:25:16 -0600

    modificaciones en pestaña MI PERFEIL

--- js/Master.js
+++ js/Master.js
@@ -16,94 +16,85 @@ $(document).ready(function(){
     });
 });
 
+
 function AgregarMenu(){
-    var menu = document.getElementById("Menu");
-    menu.innerHTML = '<h1 class="visually-hidden">Sidebars examples</h1>'+
-    '<a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">'+
-      //'<svg class="bi pe-none me-2" width="30" height="24"><use xlink:href="#bootstrap"/></svg>'+
-      //'<span class="fs-5 fw-semibold">EnigmaPOS</span>'+
-    '</a>'+
-    '<ul class="list-unstyled ps-0">'+
-    
-    //INICIO
-    '</button>'+
-    '<div class="collapse show" id="dashboard-collapse">'+
-      '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
-        '<li><a href="../forms/main.html" class="link-dark d-inline-flex text-decoration-none rounded">INICIO</a></li>'+
-       
-    //MI PERFIL
-    '<li class="mb-1">'+
-    '<button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">'+
-      
-    'MI PERFIL'+
-    '</button>'+
-    '<div class="collapse show" id="dashboard-collapse">'+
-      '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
-        '<li><a href="../forms/Preguntas.html" class="link-dark d-inline-flex text-decoration-none rounded">Mis preguntas</a></li>'+
-        '<li><a href="../forms/Cambio_Contra.html" class="link-dark d-inline-flex text-decoration-none rounded">Cambio de contraseña</a></li>'+
-        '<li><a href="../forms/editar_usuario.html" class="link-dark d-inline-flex text-decoration-none rounded">Editar usuario</a></li>'+
-        '<li><a href="../forms/cerrar_sesion.html" class="link-dark d-inline-flex text-decoration-none rounded">Cerrar sesion</a></li>'+
-'</ul>'+
-    '</div>'+
-  '</li>'+
-    
-    
-  //CONTABILIDAD
-    '<li class="mb-1">'+
-        '<button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">'+
-          'CONTABILIDAD'+
-        '</button>'+
-        '<div class="collapse show" id="home-collapse">'+
-          '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
-            '<li><a href="../forms/conta_categoria.html" class="link-dark d-inline-flex text-decoration-none rounded">Categoría contable</a></li>'+
-			'<li><a href="../forms/conta_catalogo.html" class="link-dark d-inline-flex text-decoration-none rounded">Catálogo de cuentas</a></li>'+
-			'<li><a href="../forms/conta_subcuenta.html" class="link-dark d-inline-flex text-decoration-none rounded">Subcuentas</a></li>'+
-            '<li><a href="../forms/conta_periodo.html" class="link-dark d-inline-flex text-decoration-none rounded">Período contable</a></li>'+
-            '<li><a href="../forms/conta_estadolb.html" class="link-dark d-inline-flex text-decoration-none rounded">Estado de libro diario</a></li>'+
-			'<li><a href="../forms/conta_librodiario.html" class="link-dark d-inline-flex text-decoration-none rounded">Libro diario</a></li>'+
-			'<li><a href="../forms/conta_libromayor.html" class="link-dark d-inline-flex text-decoration-none rounded">Libro mayor</a></li>'+
-		  '</ul>'+
-        '</div>'+
-      '</li>'+
-      
-
-
-      //REPORTES
-      '<li class="mb-1">'+
-        '<button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">'+
-          'REPORTES'+
-        '</button>'+
-        '<div class="collapse show" id="orders-collapse">'+
-          '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
-            '<li><a href="../forms/reporte_venta_resumen.html" class="link-dark d-inline-flex text-decoration-none rounded">Balance general</a></li>'+
-		      	'<li><a href="../forms/reporte_venta_cajero.html" class="link-dark d-inline-flex text-decoration-none rounded">Estado de resultado</a></li>'+
-            '<li><a href="../forms/reporte_venta_producto.html" class="link-dark d-inline-flex text-decoration-none rounded">Ingresos y gastos</a></li>'+
-            '</ul>'+
-        '</div>'+
-      '</li>'+
-
-//SEGURIDAD
-'<li class="mb-1">'+
-'<button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">'+
+  var menu = document.getElementById("Menu");
+  menu.innerHTML = '<h1 class="visually-hidden">Sidebars examples</h1>'+
   
-'SEGURIDAD'+
-'</button>'+
-'<div class="collapse show" id="dashboard-collapse">'+
-  '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
-    '<li><a href="../forms/seg_usuario.html" class="link-dark d-inline-flex text-decoration-none rounded">Usuario</a></li>'+
-    '<li><a href="../forms/articulo_lista.html" class="link-dark d-inline-flex text-decoration-none rounded">Gestión de usuario</a></li>'+
-    '<li><a href="../forms/lista-materiales.html" class="link-dark d-inline-flex text-decoration-none rounded">Preguntas</a></li>'+
-    '<li><a href="../forms/Unidades_medida_lista.html" class="link-dark d-inline-flex text-decoration-none rounded">Preguntas de usuario</a></li>'+
-    '<li><a href="../forms/ingresar_mercaderia.html" class="link-dark d-inline-flex text-decoration-none rounded">Parámetros</a></li>'+
-    '<li><a href="../forms/salida_mercaderia.html" class="link-dark d-inline-flex text-decoration-none rounded">Objetos</a></li>'+
-    '<li><a href="../forms/salida_mercaderia.html" class="link-dark d-inline-flex text-decoration-none rounded">Permisos</a></li>'+
-    '<li><a href="../forms/salida_mercaderia.html" class="link-dark d-inline-flex text-decoration-none rounded">Roles</a></li>'+
-'</ul>'+
-'</div>'+
-'</li>'
+     '<div class="collapse show" id="ccollapse"><a href="../forms/main.html" class="link-dark d-inline-flex text-decoration-none rounded">INICIO</a></div>'+
+     '<div class="border-top my-3"></div>'+
+    
+  '<ul class="list-unstyled ps-0">'+
+  
+    '<li class="mb-1">'+
+    //MENÚ DE MI PERFIL
+      '<button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">'+
+        'MI PERFIL'+
+      '</button>'+
+      '<div class="collapse show" id="home-collapse">'+
+        '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
+          '<li><a href="../forms/1. MI PERFIL_Preguntas.html" class="link-dark d-inline-flex text-decoration-none rounded">Mis Preguntas</a></li>'+
+    '<li><a href="../forms/1.4 Cambio_Contra.html" class="link-dark d-inline-flex text-decoration-none rounded">Cambio de Contraseña</a></li>'+
+    '<li><a href="../forms/1.5 Editar usuario.html" class="link-dark d-inline-flex text-decoration-none rounded">Editar Usuario</a></li>'+
+          '<li><a href="../forms/1.6 Cerrar sesion.html" class="link-dark d-inline-flex text-decoration-none rounded">Cerrar Sesión</a></li>'+   
+    '</ul>'+
+      '</div>'+
+    '</li>'+
 
+    //MENÚ DE CONTABILIDAD
+    '<li class="mb-1">'+
+      '<button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">'+
+        'CONTABILIDAD'+
+      '</button>'+
+      '<div class="collapse show" id="dashboard-collapse">'+
+        '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
+          '<li><a href="../forms/2. CONTABILIDAD_categoria.html" class="link-dark d-inline-flex text-decoration-none rounded">Categoría Contable</a></li>'+
+          '<li><a href="../forms/3. CONTABILIDAD_catalogo.html" class="link-dark d-inline-flex text-decoration-none rounded">Catálogo de Cuentas</a></li>'+
+          '<li><a href="../forms/4. CONTABILIDAD_subcuenta.html" class="link-dark d-inline-flex text-decoration-none rounded">Subcuentas</a></li>'+
+          '<li><a href="../forms/5. CONTABILIDAD_periodo.html" class="link-dark d-inline-flex text-decoration-none rounded">Período Contable</a></li>'+
+          '<li><a href="../forms/6. CONTABILIDAD_estado.html" class="link-dark d-inline-flex text-decoration-none rounded">Estado de Libro Diario</a></li>'+
+          '<li><a href="../forms/7. CONTABILIDAD_libro diario.html" class="link-dark d-inline-flex text-decoration-none rounded">Libro Diario</a></li>'+
+          '<li><a href="../forms/8. CONTABILIDAD_libro mayor.html" class="link-dark d-inline-flex text-decoration-none rounded">Libro Mayor</a></li>'+
+    '</ul>'+
+      '</div>'+
+    '</li>'+
 
+    //MENÚ DE REPORTES
+    '<li class="mb-1">'+
+      '<button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">'+
+        'REPORTES'+
+      '</button>'+
+      '<div class="collapse show" id="orders-collapse">'+
+        '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
+          '<li><a href="../forms/9. REPORTE_Balance General.html" class="link-dark d-inline-flex text-decoration-none rounded">Balance General</a></li>'+
+          '<li><a href="../forms/10. REPORTE_Estado Resultado.html" class="link-dark d-inline-flex text-decoration-none rounded">Estado de Resultado</a></li>'+
+          '<li><a href="../forms/11. REPORTE_Ingresos y gastos.html" class="link-dark d-inline-flex text-decoration-none rounded">Ingresos y Egresos</a></li>'+
+        '</ul>'+
+      '</div>'+
+    '</li>'+
+    
+    //MENÚ DE SEGURIDAD
+    '<li class="mb-1">'+
+      '<button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">'+
+        'SEGURIDAD'+
+      '</button>'+
+      '<div class="collapse show" id="account-collapse">'+
+        '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
+          '<li><a href="../forms/13. SEGURIDAD_usuario.html" class="link-dark d-inline-flex text-decoration-none rounded">Usuario</a></li>'+
+          '<li><a href="../forms/12. SEGURIDAD_gestión usuario.html" class="link-dark d-inline-flex text-decoration-none rounded">Gestión de Usuario</a></li>'+
+          '<li><a href="../forms/14. SEGURIDAD_preguntas.html" class="link-dark d-inline-flex text-decoration-none rounded">Preguntas</a></li>'+
+          '<li><a href="../forms/15. SEGURIDAD_pregs usuario.html" class="link-dark d-inline-flex text-decoration-none rounded">Preguntas de Usuario</a></li>'+
+          '<li><a href="../forms/16. SEGURIDAD_parámetros.html" class="link-dark d-inline-flex text-decoration-none rounded">Parámetros</a></li>'+
+          '<li><a href="../forms/17. SEGURIDAD_objetos.html" class="link-dark d-inline-flex text-decoration-none rounded">Objetos</a></li>'+
+          '<li><a href="../forms/18. SEGURIDAD_permisos.html" class="link-dark d-inline-flex text-decoration-none rounded">Permisos</a></li>'+
+          '<li><a href="../forms/19. SEGURIDAD_roles.html" class="link-dark d-inline-flex text-decoration-none rounded">Roles</a></li>'+
+          '</ul>'+
+      '</div>'+
+    '</li>'+
+  '</ul>'
 }
+
+
 
 function AgregarFooter(){
     var menu = document.getElementById("Pie");
@@ -132,7 +123,6 @@ function AgregarHeader(){
             '<img src="../images/burridogs_logo.png" alt="" width="130" height="70" class="d-inline-block align-text-mid">'+
             'Burridogs'+
           '</a>'+
-          
             '<span class="navbar-toggler-icon"></span>'+
           '</button>'+
           '<div class="collapse navbar-collapse" id="navbarSupportedContent">'+
